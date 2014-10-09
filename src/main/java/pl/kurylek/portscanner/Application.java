f67cf55e75c8126c131caf5b0884ebdb78bf0905
Tomasz Kuryłek <tkurylek@future-processing.com>
2014-10-09 11:27:56 +0200

    Added logging framework slf4j

--- src/main/java/pl/kurylek/portscanner/Application.java
+++ src/main/java/pl/kurylek/portscanner/Application.java
@@ -3,28 +3,35 @@ package pl.kurylek.portscanner;
 import jssc.SerialPort;
 import jssc.SerialPortException;
 import jssc.SerialPortList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 
 public class Application {
 
-	public static void main(String[] args) {
-		Arrays.asList(SerialPortList.getPortNames()).stream().map(SerialPort::new).forEach(Application::printSerialPortInfo);
-	}
+  private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
-	private static void printSerialPortInfo(SerialPort serialPort) {
-		try {
-			serialPort.openPort();
-			System.out.println("Port name: " + serialPort.getPortName());
-			System.out.println("CTS: " + serialPort.isCTS());
-			System.out.println("DSR: " + serialPort.isDSR());
-			System.out.println("RING: " + serialPort.isRING());
-			System.out.println("RLSD: " + serialPort.isRLSD());
-			System.out.println("Opened: "+ serialPort.isOpened());
-			System.out.println();
-			serialPort.closePort();
-		} catch (SerialPortException e) {
-			throw new RuntimeException(e);
-		}
-	}
+  public static void main(String[] args) {
+    Arrays.asList(SerialPortList.getPortNames())
+        .stream().map(SerialPort::new).forEach(Application::printSerialPortInfo);
+  }
+
+  private static void printSerialPortInfo(SerialPort serialPort) {
+    try {
+      serialPort.openPort();
+      LOGGER.info(new StringBuilder()
+          .append("Port name: ").append(serialPort.getPortName())
+          .append("\nOpened: ").append(serialPort.isOpened())
+          .append("\nClear to Send: ").append(serialPort.isCTS())
+          .append("\nData Set Ready: ").append(serialPort.isDSR())
+          .append("\nReceive Line Signal Detect: ").append(serialPort.isRLSD())
+          .append("\nRing Indicator: ").append(serialPort.isRING())
+          .append("\nFlow control mode: ").append(serialPort.getFlowControlMode())
+          .toString());
+      serialPort.closePort();
+    } catch (SerialPortException e) {
+      LOGGER.error("Communication failed!", e);
+    }
+  }
 }
